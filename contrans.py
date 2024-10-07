db4d48cf35aef38cd9ee5f80c992b679bed19a1f
jkropko <jkropko@gmail.com>
2024-10-07 12:20:21 -0400

    Added bill text and wrote get_billdata() method

--- contrans.py
+++ contrans.py
@@ -4,6 +4,7 @@ import os
 import dotenv
 import requests
 import json
+from bs4 import BeautifulSoup
 
 class contrans:
         def __init__(self):
@@ -97,14 +98,28 @@ class contrans:
                 
                 params['limit'] = 250
                 j = 0
-                bills_dict = {}
+                bills_list = []
                 while j < totalrecords:
                         params['offset'] = j
                         r = requests.get(root + endpoint,
                                          params=params,
                                          headers=headers)
                         records = r.json()['sponsoredLegislation']
-                        bills_dict = bills_dict.update(records)
+                        bills_list = bills_list + records
                         j = j + 250
 
-                return bills_dict
+                return bills_list
+        
+        def get_billdata(self, billurl):
+                r = requests.get(billurl,
+                                params = {'api_key': self.congresskey})
+                bill_json = json.loads(r.text)
+                texturl = bill_json['bill']['textVersions']['url']
+                r = requests.get(texturl,
+                                params = {'api_key': self.congresskey})
+                toscrape =json.loads(r.text)['textVersions'][0]['formats'][0]['url']
+                r = requests.get(toscrape)
+                mysoup = BeautifulSoup(r.text, 'html.parser')
+                billtext = mysoup.text 
+                bill_json['bill_text'] = billtext
+                return bill_json
